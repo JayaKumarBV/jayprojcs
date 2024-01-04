7077034e03125b2249b5998d49ae1b8cf687685f
JayakumarBXyram <jayakumar.balan@xyramsoft.com>
2024-01-04 16:46:58 +0530

    changes

--- FirstProject/src/com/jayxyram/Main.java
+++ FirstProject/src/com/jayxyram/Main.java
@@ -308,7 +308,9 @@ public class Main {
             else if (number % 3 == 0)
                     System.out.println("buzz");
             else System.out.println(number);
-/*
+
+
+      /*
         //for loops
             for(int i = 5; i > 0; i--)
                     System.out.println("Hello World"+ i);
